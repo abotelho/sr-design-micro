98aa6cb2133787a4a1c174e4b74c42cb3d8747f3
Andrew Botelho <botelhoa92@gmail.com>
2014-04-17 16:04:02 -0400

    separated definitions into header file, added tag class, write ToBT function

--- p2d2/p2d2.ino
+++ p2d2/p2d2.ino
@@ -11,60 +11,26 @@
  * bluetooth communication with a smartphone. 
  */
 
+
+#include "bluetooth.h"
+#include "Tag.h"
 #include <SoftwareSerial.h>  
-
-/*******************************************************************************
- * Definitions 
- ******************************************************************************/
-//Header bytes
-#define HEADER_IN_1 0xB8
-#define HEADER_IN_2 0xD3
-#define HEADER_OUT_1 0xFA
-#define HEADER_OUT_2 0xCE
-
-//Led status bytes
-#define LED_STATE 0x3C
-#define LED_ON 0xFF
-#define LED_OFF 0x00
-
-// Heating status bytes
-#define HEATING_STATE 0x57
-#define HEAT_TEMP_1 0x33
-#define HEAT_TEMP_2 0x55
-#define HEAT_TEMP_3 0x65
-#define HEAT_TEMP_4 0xFF
-#define STOP_HEATING 0x00
-#define HEATING_TEMP_1 0x31
-#define HEATING_TEMP_2 0x51
-#define HEATING_TEMP_3 0x62
-#define HEATING_TEMP_4 0xF7
-
-// Fluid status bytes
-#define FLUID_STATE 0x8F
-#define FLUIDS_ACTUATE 0xFF
-#define FLUIDS_STOP 0x00
-#define FLUIDS_NOT_ACTUATED 0x00
-#define FLUIDS_ACTUATING 0x44
-#define FLUIDS_ACTUATED 0xFF
-
-// Temp data byte
-#define TEMP_DATA 0x6A
-
-// Full status request bytes
-#define STATUS_REQUEST 0xFF
-#define FULL_STATUS 0xA5
 
 /******************************************************************************
  * Variable initializations
  ******************************************************************************/
-int bluetoothTx = 2;  // TX-O pin of bluetooth mate, Arduino D2
-int bluetoothRx = 3;  // RX-I pin of bluetooth mate, Arduino D3
+// pins
+const int bluetoothTx = 2;  // TX-O pin of bluetooth mate, Arduino D2
+const int bluetoothRx = 3;  // RX-I pin of bluetooth mate, Arduino D3
+const int led = 13;  // LED output pin
+
 int length; //length of packet
 int cmd; //command selection from bluetooth
 int srcmd; //status requests
 int heatingcmd; //heating commands
 int ledcmd; //led commands
 int fluidscmd; //fluid commands
+int minPacketSize = 5;
 SoftwareSerial bluetooth(bluetoothTx, bluetoothRx); // connection to Blue SmiRF
 
 // Runs before loop()
@@ -106,17 +72,18 @@ void status_requests(int cmd){
     Serial.print("//led state request");
   }
   else if(cmd == HEATING_STATE){
-   Serial.print(" //heating state request");
+    Serial.print(" //heating state request");
   }
   else if (cmd == FLUID_STATE){
-   Serial.print("//fluid acutation state request");
+    Serial.print("//fluid acutation state request");
   }
   else if (cmd == TEMP_DATA){
-   Serial.print("//temp data request");
+    Serial.print("//temp data request");
   }
   else if (cmd == FULL_STATUS){
     Serial.print("//full status (all state values and data)");
-  }else{
+  }
+  else{
     Serial.print("invalid input received");
   }
 } 
@@ -129,7 +96,7 @@ void heating_commands(int cmd){
   }
   else if(cmd == HEAT_TEMP_2){
     //Start/switch heating-- temp 2
-     Serial.print("Start/switch heating-- temp 2\r\n");
+    Serial.print("Start/switch heating-- temp 2\r\n");
   }
   else if (cmd == HEAT_TEMP_3){
     //Start/switch heating-- temp 3
@@ -149,8 +116,9 @@ void heating_commands(int cmd){
 void led_command(int cmd){
   if (cmd == LED_ON){
     Serial.print("//Leds on\r\n");
-  }else if(cmd == LED_OFF){
-     Serial.print("//Leds off\r\n");
+  }
+  else if(cmd == LED_OFF){
+    Serial.print("//Leds off\r\n");
   }
 }  
 
@@ -158,48 +126,63 @@ void led_command(int cmd){
 void fluids_command(int cmd){
   if (cmd == FLUIDS_ACTUATE){
     //Start fluids actuation
-   Serial.print("Start fluids actuation");
-  }else if(cmd == FLUIDS_STOP){
+    Serial.print("Start fluids actuation");
+  }
+  else if(cmd == FLUIDS_STOP){
     //Stop fluids actuation
-     Serial.print("Stop fluids actuation");
+    Serial.print("Stop fluids actuation");
   }
 }  
 
 // Processes packets received over bluetooth
 void processPacket(){
   if(bluetooth.read() == HEADER_IN_1){
-      //Serial.print("first byte received\r\n");
-      if(bluetooth.read() == HEADER_IN_2){
-        //Serial.print("second byte received\r\n");
-        length = bluetooth.read();
-        //Serial.print(length);
-        for( int i = 1; i <= length; i++){
-          cmd = bluetooth.read();
-          if (cmd == STATUS_REQUEST){
-            srcmd = bluetooth.read();
-            status_requests(srcmd);
-          }
-          else if (cmd == HEATING_STATE){
-            heatingcmd = bluetooth.read();
-            heating_commands(heatingcmd);
-          }
-          else if (cmd == LED_STATE){
-            ledcmd = bluetooth.read();
-            led_command(ledcmd);
-          }
-          else if (cmd == FLUID_STATE){
-            fluidscmd = bluetooth.read();
-            fluids_command(fluidscmd);
-          }
+    //Serial.print("first byte received\r\n");
+    if(bluetooth.read() == HEADER_IN_2){
+      //Serial.print("second byte received\r\n");
+      length = bluetooth.read();
+      //Serial.print(length);
+      for( int i = 1; i <= length; i++){
+        cmd = bluetooth.read();
+        if (cmd == STATUS_REQUEST){
+          srcmd = bluetooth.read();
+          status_requests(srcmd);
         }
-
-      }
-      else{
-        bluetooth.flush();
+        else if (cmd == HEATING_STATE){
+          heatingcmd = bluetooth.read();
+          heating_commands(heatingcmd);
+        }
+        else if (cmd == LED_STATE){
+          ledcmd = bluetooth.read();
+          led_command(ledcmd);
+        }
+        else if (cmd == FLUID_STATE){
+          fluidscmd = bluetooth.read();
+          fluids_command(fluidscmd);
+        }
       }
 
     }
     else{
       bluetooth.flush();
     }
+  }
+  else{
+    bluetooth.flush();
+  }
+}
+
+void writeToBT(Tag tagArray[]){
+  byte tagArraySize = sizeof(tagArray)/sizeof(Tag);
+  bluetooth.write(HEADER_OUT_1);
+  bluetooth.write(HEADER_OUT_1);
+  bluetooth.write(tagArraySize);
+  for(int i = 0; i< tagArraySize; i++){
+    byte type = tagArray[i].getType();
+    bluetooth.write(type);
+    bluetooth.write(tagArray[i].getData1());
+    if(type == TEMP_DATA){
+      bluetooth.write(tagArray[i].getData2());
+    }
+  }
 }
